318b90fce213f1bc1213fce5525358e3226d7a37
Justin McGinnis <justin.mcginnis@gmail.com>
2011-09-26 16:46:47 -0500

    Added some rdocumentation

--- lib/sigma/api.rb
+++ lib/sigma/api.rb
@@ -1,4 +1,15 @@
 module Sigma
+  # This is the base class for all request to Sigma.  When creating a new Api object, it must be initialized with one of the available Api's.
+  #
+  #   api = Sigma::Api.new( Sigma::ZipCodeLookup.new(:zip_code => "55318") )
+  #   api.transaction         => Sigma::ZipCodeLookup
+  #
+  # After the object is initiailzed however, you can swap out the Api request by setting the transaction type explicitly
+  #
+  #   api.transaction = Sigma::ProductAvailability.new(:product_number => [57337, 36774, 50093, 93939], :response_type => "M")
+  #   api.transaction         => Sigma::ProductAvailability
+  #
+  # This class doesn't do much besides pass requests on to the associated trasaction object via the method_missing method.
   class Api
     attr_accessor :transaction
 
@@ -6,6 +17,7 @@ module Sigma
       @transaction = transaction
     end
     
+    # Checks to see if the requested method exists on the trasaction object and calls it if it does, or just calls super.
     def method_missing(name, *args)
       super unless @transaction.respond_to? name
       @transaction.send name, *args
